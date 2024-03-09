564cb521e79efc98c0e473618e0f04baa7f6da0c
HildaEDIMA <160679847+HildaEDIMA@users.noreply.github.com>
2024-03-09 08:14:50 +0000

    DEVOIR 1 EDIMA.R

--- DEVOIR 1 EDIMA.R
+++ DEVOIR 1 EDIMA.R
@@ -1,11 +1,11 @@
 #DEVOIR 1 EDIMA BIYENDA HILDEGARDE ISEP2_ENSAE
 
-# Définition de la fonction
+# DÃ©finition de la fonction
 
 f <- function(x, y) {
   return(x^2 + cos(x+y)+(y^2+1/(1+x^2)^(1/2)))
 }
-# Création d'une grille de valeurs pour x et y
+# CrÃ©ation d'une grille de valeurs pour x et y
 
 x <- seq(-2, 2, length.out = 100)
 y <- seq(-2, 2, length.out = 100)
@@ -18,13 +18,13 @@ min <- min(f(x,y))
 print(paste("Le Maximum de f(x)est:", max))
 print(paste("Le Minimum de f(x)est:", min))
 
-# Calcule de la dérivée partielle par rapport à x
+# Calcule de la dÃ©rivÃ©e partielle par rapport Ã  x
 
 df_dx <- function(x, y) {
   h <- 0.00001
   return((f(x + h, y) - f(x, y)) / h)
 }
-# Calcule de la dérivée partielle par rapport à y
+# Calcule de la dÃ©rivÃ©e partielle par rapport Ã  y
 df_dy <- function(x, y) {
   h <- 0.00001
   return((f(x, y + h) - f(x, y)) / h)
@@ -37,11 +37,11 @@ r <- outer(x, y, f)
 
 # afficher la fonction f
 
-persp(x, y, r, theta = 30, phi = 30, expand = 0.5, col = "yellow", xlab = "x", ylab = "y", zlab = "f(x, y)", main = "reprÃ©sentation de f")
+persp(x, y, r, theta = 30, phi = 30, expand = 0.5, col = "yellow", xlab = "x", ylab = "y", zlab = "f(x, y)", main = "reprÃƒÂ©sentation de f")
 
-# pour la dérivée x
-persp(x, y, z, theta = 30, phi = 30, expand = 0.5, col = "red", xlab = "x", ylab = "y", zlab = "df/dx", main = "reprÃ©sentation de df/dx")
-# pour la dérivé y
-persp(x, y, t, theta = 30, phi = 30, expand = 0.5, col = "green", xlab = "x", ylab = "y", zlab = "df/dy", main = "reprÃ©sentation de df/dy")
+# pour la dÃ©rivÃ©e x
+persp(x, y, z, theta = 30, phi = 30, expand = 0.5, col = "red", xlab = "x", ylab = "y", zlab = "df/dx", main = "reprÃƒÂ©sentation de df/dx")
+# pour la dÃ©rivÃ© y
+persp(x, y, t, theta = 30, phi = 30, expand = 0.5, col = "green", xlab = "x", ylab = "y", zlab = "df/dy", main = "reprÃƒÂ©sentation de df/dy")
 
 
